13f014b6f19c6ae985f44e18a4848a512a09a17d
Douglas Belinato <douglas_belinato@hotmail.com>
2026-06-28 21:20:15 -0300

    Setting DLQ config + parallel consumers + retries

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -13,10 +13,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.json.JsonMapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String QUEUE = "temperature-monitoring.process-temperature.v1.q";
+    private static final String PROCESS_TEMPERATURE = "temperature-monitoring.process-temperature.v1";
+    public static final String QUEUE_PROCESS_TEMPERATURE =  PROCESS_TEMPERATURE + ".q";
+    public static final String DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE = PROCESS_TEMPERATURE + ".dlq";
+    public static final String QUEUE_ALERTING = "temperature-monitoring.alerting.v1.q";
 
     @Bean
     public JacksonJsonMessageConverter messageConverter(JsonMapper jsonMapper) {
@@ -29,8 +35,21 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Queue queue() {
-        return QueueBuilder.durable(QUEUE).build();
+    public Queue queueProcessTemperature() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "");
+        args.put("x-dead-letter-routing-key", DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE);
+        return QueueBuilder.durable(QUEUE_PROCESS_TEMPERATURE).withArguments(args).build();
+    }
+
+    @Bean
+    public Queue deadLetterQueueProcessTemperature() {
+        return QueueBuilder.durable(DEAD_LETTER_QUEUE_PROCESS_TEMPERATURE).build();
+    }
+
+    @Bean
+    public Queue queueAlerting() {
+        return QueueBuilder.durable(QUEUE_ALERTING).build();
     }
 
     public FanoutExchange exchange() {
@@ -38,8 +57,13 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Binding binding() {
-        return BindingBuilder.bind(queue()).to(exchange());
+    public Binding bindingProcessTemperature() {
+        return BindingBuilder.bind(queueProcessTemperature()).to(exchange());
+    }
+
+    @Bean
+    public Binding bindingAlerting() {
+        return BindingBuilder.bind(queueAlerting()).to(exchange());
     }
 
 }
--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQListener.java
@@ -1,32 +1,39 @@
 package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq;
 
 import com.algaworks.algasensors.temperature.monitoring.api.model.TemperatureLogData;
-import io.hypersistence.tsid.TSID;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.SensorAlertService;
+import com.algaworks.algasensors.temperature.monitoring.domain.service.TemperatureMonitoringService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.time.Duration;
 
-import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE;
+import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE_ALERTING;
+import static com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq.RabbitMQConfig.QUEUE_PROCESS_TEMPERATURE;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RabbitMQListener {
 
-    @RabbitListener(queues = QUEUE, concurrency = "2-3")
+    private final TemperatureMonitoringService temperatureMonitoringService;
+    private final SensorAlertService sensorAlertService;
+
+    @RabbitListener(queues = QUEUE_PROCESS_TEMPERATURE, concurrency = "2-3")
     @SneakyThrows
-    public void handle(@Payload TemperatureLogData temperatureLogData, @Headers Map<String, String> headers) {
-        TSID sensorId = temperatureLogData.getSensorId();
-        Double temperature = temperatureLogData.getValue();
-        log.info("Temperature updated: SensorId {} Temp {}", sensorId,  temperature);
-        log.info("Headers: {}", headers);
-        Thread.sleep(5000);
+    public void handleProcessingTemperature(@Payload TemperatureLogData temperatureLogData) {
+        temperatureMonitoringService.processTemperatureReading(temperatureLogData);
+    }
+
+    @RabbitListener(queues = QUEUE_ALERTING, concurrency = "2-3")
+    @SneakyThrows
+    public void handleAlerting(@Payload TemperatureLogData temperatureLogData) {
+        sensorAlertService.handleAlert(temperatureLogData);
+        Thread.sleep(Duration.ofSeconds(5));
     }
 
 }
